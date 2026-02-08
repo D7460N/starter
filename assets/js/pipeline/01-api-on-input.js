e07433918d198a256c80640cca9c11d2bd4f3908
D7460N <80736+dragontheory@users.noreply.github.com>
2026-02-07 22:02:28 -0500

    Add commented API endpoints to 01-api-on-input.js
    Added commented API endpoints for reference.

--- assets/js/pipeline/01-api-on-input.js
+++ assets/js/pipeline/01-api-on-input.js
@@ -10,3 +10,14 @@ function apiOnInput(url, options = {}) {
       return null;
     });
 }
+// https://6987f917780e8375a6874dcf.mockapi.io/nav
+// https://6987f917780e8375a6874dcf.mockapi.io/home
+// https://6987f917780e8375a6874dcf.mockapi.io/about
+// https://6987f917780e8375a6874dcf.mockapi.io/events
+// https://6987f917780e8375a6874dcf.mockapi.io/products
+// https://6987f917780e8375a6874dcf.mockapi.io/contact
+
+// https://6987f917780e8375a6874dcf.mockapi.io/banner-top
+// https://6987f917780e8375a6874dcf.mockapi.io/header
+// https://6987f917780e8375a6874dcf.mockapi.io/footer
+// https://6987f917780e8375a6874dcf.mockapi.io/banner-bottom
